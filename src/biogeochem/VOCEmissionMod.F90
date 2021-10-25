7d05e47e1918235fbd5027c4d0ad1ffd04b96fe8
Erik Kluzek <erik@ucar.edu>
2021-10-25 15:14:46 -0600

    Remove contents of vocemis and aerosol types

--- src/biogeochem/VOCEmissionMod.F90
+++ src/biogeochem/VOCEmissionMod.F90
@@ -7,29 +7,6 @@ module VOCEmissionMod
   ! !USES:
   use shr_kind_mod       , only : r8 => shr_kind_r8
   use shr_log_mod        , only : errMsg => shr_log_errMsg
-  use clm_varctl         , only : iulog
-  use clm_varpar         , only : numpft, nlevcan
-  use pftconMod          , only : ndllf_evr_tmp_tree,  ndllf_evr_brl_tree
-  use pftconMod          , only : ndllf_dcd_brl_tree,  nbrdlf_evr_trp_tree
-  use pftconMod          , only : nbrdlf_evr_tmp_tree, nbrdlf_dcd_brl_shrub
-  use pftconMod          , only : nbrdlf_dcd_trp_tree, nbrdlf_dcd_tmp_tree
-  use pftconMod          , only : nbrdlf_dcd_brl_tree, nbrdlf_evr_shrub
-  use pftconMod          , only : nc3_arctic_grass   , nc3crop
-  use pftconMod          , only : nc4_grass,           noveg
-  use shr_megan_mod      , only : shr_megan_megcomps_n, shr_megan_megcomp_t, shr_megan_linkedlist
-  use shr_megan_mod      , only : shr_megan_mechcomps_n, shr_megan_mechcomps, shr_megan_mapped_emisfctrs
-  use MEGANFactorsMod    , only : Agro, Amat, Anew, Aold, betaT, ct1, ct2, LDF, Ceo
-  use decompMod          , only : bounds_type
-  use abortutils         , only : endrun
-  use fileutils          , only : getfil
-  use clm_varcon         , only : grlnd
-  use atm2lndType        , only : atm2lnd_type
-  use CanopyStateType    , only : canopystate_type
-  use PhotosynthesisMod  , only : photosyns_type
-  use SoilStateType      , only : soilstate_type
-  use SolarAbsorbedType  , only : solarabs_type
-  use TemperatureType    , only : temperature_type
-  use PatchType          , only : patch                
   !
   implicit none
   private 
@@ -38,37 +15,8 @@ module VOCEmissionMod
   !
   ! !PUBLIC TYPES:
   type, public :: vocemis_type
-     real(r8) , pointer, private :: Eopt_out_patch    (:)   ! Eopt coefficient
-     real(r8) , pointer, private :: topt_out_patch    (:)   ! topt coefficient
-     real(r8) , pointer, private :: alpha_out_patch   (:)   ! alpha coefficient
-     real(r8) , pointer, private :: cp_out_patch      (:)   ! cp coefficient
-     real(r8) , pointer, private :: paru_out_patch    (:)   ! 
-     real(r8) , pointer, private :: par24u_out_patch  (:)   ! 
-     real(r8) , pointer, private :: par240u_out_patch (:)   !  
-     real(r8) , pointer, private :: para_out_patch    (:)   ! 
-     real(r8) , pointer, private :: par24a_out_patch  (:)   ! 
-     real(r8) , pointer, private :: par240a_out_patch (:)   ! 
-     real(r8) , pointer, private :: gamma_out_patch   (:)   ! 
-     real(r8) , pointer, private :: gammaL_out_patch  (:)   ! 
-     real(r8) , pointer, private :: gammaT_out_patch  (:)   ! 
-     real(r8) , pointer, private :: gammaP_out_patch  (:)   ! 
-     real(r8) , pointer, private :: gammaA_out_patch  (:)   ! 
-     real(r8) , pointer, private :: gammaS_out_patch  (:)   ! 
-     real(r8) , pointer, private :: gammaC_out_patch  (:)   ! 
-     real(r8) , pointer, private :: vocflx_tot_patch  (:)   ! total VOC flux into atmosphere [moles/m2/sec] 
-     real(r8) , pointer, PUBLIC  :: vocflx_patch      (:,:) ! (num_mech_comps) MEGAN flux [moles/m2/sec] 
-     real(r8) , pointer, private :: efisop_grc        (:,:) ! gridcell isoprene emission factors
   end type vocemis_type
   !
-  ! !PRIVATE TYPES:
-  type :: megan_out_type
-     ! VOC fluxes structure for CLM history output
-     real(r8), pointer, private  :: flux_out(:)   ! patch MEGAN flux [ug C m-2 h-1]
-  end type megan_out_type
-  type(megan_out_type), private, pointer :: meg_out(:) ! (n_megan_comps) points to output fluxes
-  !
-  logical, parameter :: debug = .false.
-
   character(len=*), parameter, private :: sourcefile = &
        __FILE__
   !------------------------------------------------------------------------
--- src/biogeophys/AerosolMod.F90
+++ src/biogeophys/AerosolMod.F90
@@ -6,13 +6,6 @@ module AerosolMod
   use shr_kind_mod     , only : r8 => shr_kind_r8
   use shr_log_mod      , only : errMsg => shr_log_errMsg
   use shr_infnan_mod   , only : nan => shr_infnan_nan, assignment(=)
-  use decompMod        , only : bounds_type
-  use clm_varpar       , only : nlevsno, nlevgrnd 
-  use clm_time_manager , only : get_step_size
-  use atm2lndType      , only : atm2lnd_type
-  use WaterfluxType    , only : waterflux_type
-  use WaterstateType   , only : waterstate_type
-  use ColumnType       , only : col               
   use abortutils       , only : endrun
   !
   ! !PUBLIC TYPES:
@@ -26,56 +19,6 @@ module AerosolMod
   real(r8), public            :: fresh_snw_rds_max = 204.526_r8   ! maximum warm fresh snow effective radius [microns]
   !
   type, public :: aerosol_type
-     real(r8), pointer, public  :: mss_bcpho_col(:,:)      ! mass of hydrophobic BC in snow (col,lyr)     [kg]
-     real(r8), pointer, public  :: mss_bcphi_col(:,:)      ! mass of hydrophillic BC in snow (col,lyr)    [kg]
-     real(r8), pointer, public  :: mss_bctot_col(:,:)      ! total mass of BC in snow (pho+phi) (col,lyr) [kg]
-     real(r8), pointer, public  :: mss_bc_col_col(:)       ! column-integrated mass of total BC           [kg]
-     real(r8), pointer, public  :: mss_bc_top_col(:)       ! top-layer mass of total BC                   [kg]
-
-     real(r8), pointer, public  :: mss_ocpho_col(:,:)      ! mass of hydrophobic OC in snow (col,lyr)     [kg]
-     real(r8), pointer, public  :: mss_ocphi_col(:,:)      ! mass of hydrophillic OC in snow (col,lyr)    [kg]
-     real(r8), pointer, public  :: mss_octot_col(:,:)      ! total mass of OC in snow (pho+phi) (col,lyr) [kg]
-     real(r8), pointer, public  :: mss_oc_col_col(:)       ! column-integrated mass of total OC           [kg]
-     real(r8), pointer, public  :: mss_oc_top_col(:)       ! top-layer mass of total OC                   [kg]
-
-     real(r8), pointer, public  :: mss_dst1_col(:,:)       ! mass of dust species 1 in snow (col,lyr)     [kg]
-     real(r8), pointer, public  :: mss_dst2_col(:,:)       ! mass of dust species 2 in snow (col,lyr)     [kg]
-     real(r8), pointer, public  :: mss_dst3_col(:,:)       ! mass of dust species 3 in snow (col,lyr)     [kg]
-     real(r8), pointer, public  :: mss_dst4_col(:,:)       ! mass of dust species 4 in snow (col,lyr)     [kg]
-     real(r8), pointer, public  :: mss_dsttot_col(:,:)     ! total mass of dust in snow (col,lyr)         [kg]
-     real(r8), pointer, public  :: mss_dst_col_col(:)      ! column-integrated mass of dust in snow       [kg]
-     real(r8), pointer, public  :: mss_dst_top_col(:)      ! top-layer mass of dust in snow               [kg]
-
-     real(r8), pointer, public  :: mss_cnc_bcphi_col(:,:)  ! mass concentration of hydrophilic BC in snow (col,lyr) [kg/kg]
-     real(r8), pointer, public  :: mss_cnc_bcpho_col(:,:)  ! mass concentration of hydrophilic BC in snow (col,lyr) [kg/kg]
-     real(r8), pointer, public  :: mss_cnc_ocphi_col(:,:)  ! mass concentration of hydrophilic OC in snow (col,lyr) [kg/kg]
-     real(r8), pointer, public  :: mss_cnc_ocpho_col(:,:)  ! mass concentration of hydrophilic OC in snow (col,lyr) [kg/kg]
-     real(r8), pointer, public  :: mss_cnc_dst1_col(:,:)   ! mass concentration of dust species 1 in snow (col,lyr) [kg/kg]
-     real(r8), pointer, public  :: mss_cnc_dst2_col(:,:)   ! mass concentration of dust species 2 in snow (col,lyr) [kg/kg]
-     real(r8), pointer, public  :: mss_cnc_dst3_col(:,:)   ! mass concentration of dust species 3 in snow (col,lyr) [kg/kg]
-     real(r8), pointer, public  :: mss_cnc_dst4_col(:,:)   ! mass concentration of dust species 4 in snow (col,lyr) [kg/kg]
-
-     real(r8), pointer, private :: flx_dst_dep_dry1_col(:) ! dust species 1 dry   deposition on ground (positive definite) [kg/s]
-     real(r8), pointer, private :: flx_dst_dep_wet1_col(:) ! dust species 1 wet   deposition on ground (positive definite) [kg/s]
-     real(r8), pointer, private :: flx_dst_dep_dry2_col(:) ! dust species 2 dry   deposition on ground (positive definite) [kg/s]
-     real(r8), pointer, private :: flx_dst_dep_wet2_col(:) ! dust species 2 wet   deposition on ground (positive definite) [kg/s]
-     real(r8), pointer, private :: flx_dst_dep_dry3_col(:) ! dust species 3 dry   deposition on ground (positive definite) [kg/s]
-     real(r8), pointer, private :: flx_dst_dep_wet3_col(:) ! dust species 3 wet   deposition on ground (positive definite) [kg/s]
-     real(r8), pointer, private :: flx_dst_dep_dry4_col(:) ! dust species 4 dry   deposition on ground (positive definite) [kg/s]
-     real(r8), pointer, private :: flx_dst_dep_wet4_col(:) ! dust species 4 wet   deposition on ground (positive definite) [kg/s]
-     real(r8), pointer, private :: flx_dst_dep_col(:)      ! total (dry+wet) dust deposition on ground (positive definite) [kg/s]
-
-     real(r8), pointer, private :: flx_bc_dep_dry_col(:)   ! dry (BCPHO+BCPHI) BC deposition on ground (positive definite) [kg/s]
-     real(r8), pointer, private :: flx_bc_dep_wet_col(:)   ! wet (BCPHI) BC       deposition on ground (positive definite) [kg/s]
-     real(r8), pointer, private :: flx_bc_dep_pho_col(:)   ! hydrophobic BC       deposition on ground (positive definite) [kg/s]
-     real(r8), pointer, private :: flx_bc_dep_phi_col(:)   ! hydrophillic BC      deposition on ground (positive definite) [kg/s]
-     real(r8), pointer, private :: flx_bc_dep_col(:)       ! total (dry+wet) BC   deposition on ground (positive definite) [kg/s]
-
-     real(r8), pointer, private :: flx_oc_dep_dry_col(:)   ! dry (OCPHO+OCPHI) OC deposition on ground (positive definite) [kg/s]
-     real(r8), pointer, private :: flx_oc_dep_wet_col(:)   ! wet (OCPHI) OC       deposition on ground (positive definite) [kg/s]
-     real(r8), pointer, private :: flx_oc_dep_pho_col(:)   ! hydrophobic OC       deposition on ground (positive definite) [kg/s]
-     real(r8), pointer, private :: flx_oc_dep_phi_col(:)   ! hydrophillic OC      deposition on ground (positive definite) [kg/s]
-     real(r8), pointer, private :: flx_oc_dep_col(:)       ! total (dry+wet) OC   deposition on ground (positive definite) [kg/s]
 
   end type aerosol_type
 
