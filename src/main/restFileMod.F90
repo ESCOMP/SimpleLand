9f6a21dc72cc547f60e13df086368fdb7ae1ccfd
Erik Kluzek <erik@ucar.edu>
2022-10-11 20:21:20 -0600

    Abort if the finidat_consistency_checks wasn't found

--- src/main/restFileMod.F90
+++ src/main/restFileMod.F90
@@ -838,7 +838,7 @@ contains
              call endrun(msg='ERROR reading finidat_consistency_checks namelist'//errMsg(sourcefile, __LINE__))
           end if
        else
-          write(iulog,*) 'Could not find finidat_consistency_checks namelist'
+          call endrun(msg='Could not find finidat_consistency_checks namelist'//errMsg(sourcefile, __LINE__))
        end if
        close(nu_nml)
        call relavu( nu_nml )
